55eddbd479c4f54ba693596bdc75545fdd02a95a
mannireis <90857553+mannireis@users.noreply.github.com>
2026-05-10 18:25:32 +0100

    I added a font yay

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,8 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://mannireis.com",
   prefetch: {
     defaultStrategy: "viewport",
-  },
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.fontsource(),
-        name: "Lexend",
-        cssVariable: "--font-lexend",
-      },
-    ],
   },
 });
