dd39fd00bc5775202ff5985a0261d55fdfdf3a6b
mannireis <90857553+mannireis@users.noreply.github.com>
2026-05-11 13:51:13 +0100

    meow :3 this should work better images and extra stuff

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# website
-my personal website :3
+# This is my awesome personal website!
+
+I have a few blog's and project's and silly stuff in this website so heres a image :3
+
+![photo](/images/website.png)
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,7 +9,7 @@ export const projects = [
   {
     title: "Relic Keyboard",
     description: "A split keyboard i use everyday",
-    image: "/images/keyboard.jpg",
+    image: "/images/keyboard.png",
     github: "https://github.com/mannireis/relic",
     tags: ["featured", "hardware", "complete"] as const,
   },
@@ -17,8 +17,15 @@ export const projects = [
     title: "Subway Surface",
     description: "My submision for campfire lisbon",
     image: "/images/subway-surface.png",
-    github: "https://github.com/mannireis",
+    github: "github.com/mannireis/campfire",
     tags: ["featured", "software", "paused"] as const,
+  },
+  {
+    title: "Rhthim game",
+    description: "A awesome game im making",
+    image: "/images/under-construction.png",
+    github: "github.com/mannireis/",
+    tags: ["software", "paused"] as const,
   },
 ];
 
